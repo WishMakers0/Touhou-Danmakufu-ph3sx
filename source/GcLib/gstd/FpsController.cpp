8699e27192874aa8429be81e8c5a880de73add0e
Natashi <32347635+Natashi@users.noreply.github.com>
2020-04-26 23:39:26 +0700

    Removed problematic code

--- source/GcLib/gstd/FpsController.cpp
+++ source/GcLib/gstd/FpsController.cpp
@@ -20,16 +20,14 @@ FpsController::~FpsController() {
 int FpsController::_GetTime() {
 	//	int res = ::timeGetTime();
 
-	/*
 	LARGE_INTEGER nFreq;
 	LARGE_INTEGER nCounter;
 	QueryPerformanceFrequency(&nFreq);
 	QueryPerformanceCounter(&nCounter);
-	int res = (DWORD)(nCounter.QuadPart * 1000 / nFreq.QuadPart);
-	*/
+	return (DWORD)(nCounter.QuadPart * 1000 / nFreq.QuadPart);
 
-	auto ms = std::chrono::time_point_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now()).time_since_epoch();
-	return std::chrono::duration_cast<std::chrono::milliseconds>(ms).count();
+	//auto ms = std::chrono::time_point_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now()).time_since_epoch();
+	//return std::chrono::duration_cast<std::chrono::milliseconds>(ms).count();
 }
 void FpsController::_Sleep(int msec) {
 	::Sleep(msec);
